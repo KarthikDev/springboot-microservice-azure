1ee502344bf53e81e98b5511ae42af41bb3f7bba
KarthikDev <clickforkarthik@gmail.com>
2020-08-23 14:06:17 +0200

    Azure Webapp Springboot Microservice

---
+++ src/main/java/com/learn/microservices/springbootmicroserviceazure/SpringbootMicroserviceAzureApplication.java
@@ -0,0 +1,13 @@
+package com.learn.microservices.springbootmicroserviceazure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMicroserviceAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMicroserviceAzureApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/learn/microservices/springbootmicroserviceazure/controller/GreetingController.java
@@ -0,0 +1,16 @@
+package com.learn.microservices.springbootmicroserviceazure.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	@GetMapping(value = "/{name}")
+	public String sayHello(@PathVariable String name) {
+		
+		return "Hello "+name+"! Welcome to Azure Webapp Service";
+	}
+	
+}
